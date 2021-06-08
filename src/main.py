4ae4c89ec06fd147b551d8e05f62a46d4bbd14e6
Eduardo Marim Rodrigues <edumarim.br@gmail.com>
2021-06-08 17:12:51 +0100

    Start of led implementation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 from machine import Pin
 import utime
 x = 0  # control variable
+y = 0  # control variabel
 steps = 0  # steps counter
 voltas = 1
 speed = 2  # regulates the speed(the lower the faster)
@@ -57,11 +58,15 @@ def stop_go():
 t = 0
 button_right = Pin(22, Pin.IN, Pin.PULL_UP)
 button_left = Pin(19, Pin.IN, Pin.PULL_UP)
+red_led = Pin(5, Pin.IN, Pin.PULL_UP)
+yellow_led = Pin(9, Pin.IN, Pin.PULL_UP)
+green_led = Pin(10, Pin.IN, Pin.PULL_UP)
 
 while True:
     while x == 0:
-        steps_gone = 0
+        y = 0
         if button_left.value() == 0:  # when left button pressed opens the gate fully
+            y = 1
             utime.sleep_ms(500)  # small delay to start spinning
             for j in range(int(voltas*509)):
                 spin(A_CLOCK)
@@ -72,13 +77,16 @@ while True:
         x = 1
 
         while button_right.value() == 0:        # while the right button is pressed it opens
+            y = 1
             spin(A_CLOCK)
             steps += 1
             if button_right.value() == 1:
                 x = 1  # when released changes state
     while x == 1:
+        y = 0
         t = utime.time()
         while steps != steps_gone:
+            y = 1
             utime.sleep_ms(500)
 
             if button_right.value() == 0 or button_left.value() == 0:
